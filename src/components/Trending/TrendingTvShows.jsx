6bdc283658fcc1264acc6e578aa5104c126e8310
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-09-05 16:08:23 +0300

    makeStyle removed

--- src/components/Trending/TrendingTvShows.jsx
+++ src/components/Trending/TrendingTvShows.jsx
@@ -4,26 +4,13 @@ import {
   CardMedia,
   getScopedCssBaselineUtilityClass,
 } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import React from "react";
-
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
-    transitionProperty: "transform",
-    transitionDelay: "10s",
-
-    "&:hover": {
-      transform: "scale(1.1)",
-    },
-  },
-});
 
 export default function TrendingTvShows({ trending }) {
   const classes = useStyles();
   const imgPath = "https://image.tmdb.org/t/p/w500" + trending.poster_path;
   return (
-    <Card className={classes.card}>
+    <Card>
       <CardMedia component="img" image={imgPath} alt={trending.title} />
     </Card>
   );
