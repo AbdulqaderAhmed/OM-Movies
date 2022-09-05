5679beb10459d66f4e9bd70dbd69e98671fb8fdc
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-09-05 16:33:45 +0300

    header

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -64,9 +64,9 @@ export default function Header(props) {
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
           >
-            Ocean Of Game
+            Ocean Of Games
           </Typography>
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
