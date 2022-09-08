e839e06493db62f74a2142bd0488c3797cb746e7
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-09-08 22:46:57 +0300

    adding switch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import Trending from "../Trending/Trending";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import TrendingTvShows from "../Trending/TrendingTvShows";
 
 const API_KEY = "2101faa422ed30d7f1f0fcc2d9cc19f7";
 
@@ -10,9 +9,6 @@ export default function Home() {
     <>
       {/* trending movies */}
       <Trending apikey={API_KEY} />
-
-      {/* trending tv shows */}
-      <TrendingTvShows apikey={API_KEY} />
     </>
   );
 }
--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,15 +1,30 @@
-import { Card, Typography, CardMedia, Grid } from "@mui/material";
+import {
+  Card,
+  Typography,
+  CardMedia,
+  Grid,
+  Switch,
+  FormGroup,
+  FormControlLabel,
+} from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ReactOwlCarousel from "react-owl-carousel";
 
 export default function Trending({ apikey }) {
   const [movies, setMovies] = useState([]);
-  const urlTrendingMovie = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apikey}`;
+  const [checked, setChecked] = useState(false);
+  const [time, setTime] = useState("day");
+  const urlTrendingMovie = `https://api.themoviedb.org/3/trending/all/${time}?api_key=${apikey}`;
 
   const fetchMovie = async () => {
     const res = await axios.get(urlTrendingMovie);
     setMovies(res.data.results);
+  };
+
+  const handleChange = (e) => {
+    setChecked(e.target.checked);
+    console.log(checked, time);
   };
 
   useEffect(() => {
@@ -18,36 +33,38 @@ export default function Trending({ apikey }) {
 
   return (
     <>
-      <Typography variant="h5" component="h2" sx={{ mt: 10, mb: 2 }}>
-        Trending Movie
+      <Typography
+        variant="h5"
+        component="h2"
+        sx={{ mt: 10, mb: 2, display: "inline-flex" }}
+      >
+        Trendings
       </Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={checked} onChange={handleChange} />}
+          label={checked === true ? "Week" : "Day"}
+        />
+      </FormGroup>
 
       <Grid container spacing={2}>
-        <ReactOwlCarousel
-          items={5}
-          className="owl-theme"
-          loop
-          autoplay={true}
-          margin={12}
-        >
-          {movies
-            ? movies.map((movie, index) => {
-                return (
-                  <Grid item key={index}>
-                    <Card>
-                      <CardMedia
-                        component="img"
-                        image={
-                          "https://image.tmdb.org/t/p/w500" + movie.poster_path
-                        }
-                        alt={movie.title}
-                      />
-                    </Card>
-                  </Grid>
-                );
-              })
-            : null}
-        </ReactOwlCarousel>
+        {movies
+          ? movies.map((movie, index) => {
+              return (
+                <Grid key={index}>
+                  <Card sx={{ maxHeight: 450, margin: 2 }}>
+                    <CardMedia
+                      component="img"
+                      image={
+                        "https://image.tmdb.org/t/p/w300" + movie.poster_path
+                      }
+                      alt={movie.title}
+                    />
+                  </Card>
+                </Grid>
+              );
+            })
+          : null}
       </Grid>
     </>
   );
